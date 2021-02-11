f7141b38ff1fdc0f4f206ade8394b3f6eb4864cf
andrii.vivcharyk andrii.vivcharyk@vaimo.com <andrii.vivcharyk@vaimo.com>
2021-02-11 12:55:28 +0200

    Created "Brand List" and "Single Brand" pages

---
+++ app/code/Vaimo/Brand/Block/Brand.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Vaimo\Brand\Block;
+
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Api\SortOrder;
+use Magento\Framework\Api\SortOrderBuilder;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Vaimo\Brand\Api\BrandRepositoryInterface;
+
+class Brand extends Template
+{
+    /**
+     * Request
+     * @var RequestInterface
+     */
+    private $request;
+    /**
+     * Product repository
+     * @var BrandRepositoryInterface
+     */
+    private $brandRepository;
+    /**
+     * SearchCriteria builder
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+    /**
+     * SortOrder builder
+     * @var SortOrderBuilder
+     */
+    private $sortOrderBuilder;
+
+    /**
+     * BrandList constructor
+     *
+     * @param RequestInterface $request
+     * @param BrandRepositoryInterface $brandRepository
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param SortOrderBuilder $sortOrderBuilder
+     * @param Context $context
+     * @param array $data
+     */
+    public function __construct(
+        RequestInterface $request,
+        BrandRepositoryInterface $brandRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        SortOrderBuilder $sortOrderBuilder,
+        Context $context,
+        array $data = []
+    ) {
+        $this->request = $request;
+        $this->brandRepository = $brandRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->sortOrderBuilder = $sortOrderBuilder;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return \Vaimo\Brand\Api\Data\BrandInterface[]
+     */
+    public function getBrands()
+    {
+        $sortOrder = $this->sortOrderBuilder
+            ->setField('brand_id')
+            ->setDirection(SortOrder::SORT_ASC)
+            ->create();
+        $this->searchCriteriaBuilder->addSortOrder($sortOrder);
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+
+        return $this->brandRepository
+            ->getList($searchCriteria)
+            ->getItems();
+    }
+
+    /**
+     * @return \Vaimo\Brand\Api\Data\BrandInterface|null
+     */
+    public function getBrand()
+    {
+        $id = (int)$this->request->getParam('id');
+        try {
+            $result = $this->brandRepository->getById($id);
+        } catch (NoSuchEntityException $exception) {
+            $result = null;
+        }
+
+        return $result;
+    }
+}
